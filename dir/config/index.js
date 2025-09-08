a164f6573690a8710247f7c04d5d01f23c4d00ac
ADMIN <ADMIN@DESKTOP-P757OK8>
2025-09-08 15:30:25 +0530

    Remove unnecessary dir folder

--- dir/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Configuration {
-    constructor(port) {
-        this.port = port;
-    }
-}
-const configuration = new Configuration(3000);
-exports.default = configuration;
--- dir/services/home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthCheck = void 0;
-const healthCheck = (req, res) => {
-    res.send("Health Check Passed");
-};
-exports.healthCheck = healthCheck;
